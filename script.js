21f71086eda946f2a9f0f7d028f0ad80f1b7c736
Ксения Храмова <582600600@mail.ru>
2023-11-19 18:03:07 +0500

    new

---
+++ script.js
@@ -0,0 +1,20 @@
+let display = document.getElementById('input-display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'CA':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
